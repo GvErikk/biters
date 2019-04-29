4164492ca386f20aab851c50b2209e0f321434d9
Erik van Gemert <erik_v_gemert@live.nl>
2019-04-29 21:51:17 +0200

    Added model restaurant

---
+++ plugins/twor/biters/models/Restaurant.php
@@ -0,0 +1,37 @@
+<?php namespace TwoR\Biters\Models;
+
+use Model;
+
+/**
+ * Restaurant Model
+ */
+class Restaurant extends Model
+{
+    /**
+     * @var string The database table used by the model.
+     */
+    public $table = 'twor_biters_restaurants';
+
+    /**
+     * @var array Guarded fields
+     */
+    protected $guarded = ['*'];
+
+    /**
+     * @var array Fillable fields
+     */
+    protected $fillable = [];
+
+    /**
+     * @var array Relations
+     */
+    public $hasOne = [];
+    public $hasMany = [];
+    public $belongsTo = [];
+    public $belongsToMany = [];
+    public $morphTo = [];
+    public $morphOne = [];
+    public $morphMany = [];
+    public $attachOne = [];
+    public $attachMany = [];
+}
