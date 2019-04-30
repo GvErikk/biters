faa45a01eee518a72baa095ca6a1b180493e9ecd
Erik van Gemert <erik_v_gemert@live.nl>
2019-04-30 12:56:17 +0200

    Seeding tables

--- plugins/twor/biters/updates/seed_all_tables.php
+++ plugins/twor/biters/updates/seed_all_tables.php
@@ -11,5 +11,9 @@ use October\Rain\Database\Updates\Seeder;
 class SeedAllTables extends  Seeder{
     public function run(){
         Restaurant::create(['Name' => 'Wolfslaar Restaurant', 'City'=>'Breda', 'Postalcode'=>'4834 SP', 'Street' => 'Wolfslaardreef', 'Number' => '100']);
+        Restaurant::create(['Name' => 'Restaurant Zuyd', 'City'=>'Breda', 'Postalcode'=>'4818 JA', 'Street' => 'Ginnekenweg', 'Number' => '35']);
+        Restaurant::create(['Name' => 'Colins Breda', 'City'=>'Breda', 'Postalcode'=>'4818 JE', 'Street' => 'Ginnekenweg', 'Number' => '24']);
+        Restaurant::create(['Name' => 'Tante Betsie', 'City'=>'Breda', 'Postalcode'=>'4811 ZL', 'Street' => 'Sint Janstraat', 'Number' => '16']);
+        Restaurant::create(['Name' => 'Turks Restaurant Lades', 'City'=>'Breda', 'Postalcode'=>'4811 HS', 'Street' => 'Oude Vest', 'Number' => '19']);
     }
 }
